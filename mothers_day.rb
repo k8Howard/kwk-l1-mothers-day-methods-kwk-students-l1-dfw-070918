003a6fc09ee8b38d6f32d200f59115d7399b2035
k8Howard <sweetk8@me.com>
2018-07-10 18:34:32 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,4 +1,5 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
-def mothers_day
-  
+def mothers_day (Mom = Lidian)
+  "Happy Mother's Day, #{Mom}!"
+end
